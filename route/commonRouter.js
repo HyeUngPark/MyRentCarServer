c8b4bbce96ef52881566bd72f43e31ec83f0b0fb
HyeUngPark <phu8460@gmail.com>
2019-06-02 15:51:36 +0900

    google auth file fix

--- route/commonRouter.js
+++ route/commonRouter.js
@@ -2,6 +2,7 @@
     공통 라우팅
 */
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 router.get('/', function(req, res) {
@@ -9,4 +10,17 @@ router.get('/', function(req, res) {
     // console.log("job ===\n",req.job);
     // res.json({login:'success'});
 });
+
+router.get('/googlec9bbba10a3ea94c1.html', function(req, res) {
+    console.log("/호출!!!");
+    fs.readFile('googlec9bbba10a3ea94c1.html',function(err, data){
+        res.writeHead(200, {'content-Type':'text/html'});
+        res.end(data)
+    });
+    // console.log("job ===\n",req.job);
+    // res.json({login:'success'});
+});
+
+
+
 module.exports = router;
