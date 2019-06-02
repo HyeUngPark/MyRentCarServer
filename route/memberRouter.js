6389d54ffc507d715466c8c34682e7e7e8a39e08
HyeUngPark <phu8460@gmail.com>
2019-06-02 14:42:19 +0900

    random Util & gmail send method adding

--- route/memberRouter.js
+++ route/memberRouter.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var schema = require('../schema/commonSchema');
+var random = require('../myUtils/random');
+var nodemailer = require('nodemailer');
 
 router.get('/idCheck', function(req, res) {
     var params = req.query;
@@ -24,26 +26,27 @@ router.get('/idCheck', function(req, res) {
     });
 });
 
-    router.post('/login', function(req, res) {
-        console.log("/login");
-        var params = req.query;
+router.post('/login', function(req, res) {
+    console.log("/login");
+    var params = req.query;
 
-        schema.find({
-            workSection: 'MEMBER',
-            "subSchema.id": params.loginId,
-            "subSchema.pw": params.password
-        }, function(err, result) {
-            if (err) {
-                console.log('error \n', err);
-                return res.status(500).send("select error >> " + err)
-            }
-            if (result.length > 0) {
-                res.json({ "returnCode": '01', name: result[0].subSchema.name });
-            } else {
-                res.json({ "returnCode": "02" })
-            }
-        });
+    schema.find({
+        workSection: 'MEMBER',
+        "subSchema.id": params.loginId,
+        "subSchema.pw": params.password
+    }, function(err, result) {
+        if (err) {
+            console.log('error \n', err);
+            return res.status(500).send("select error >> " + err)
+        }
+        if (result.length > 0) {
+            res.json({ "returnCode": '01', name: result[0].subSchema.name });
+        } else {
+            res.json({ "returnCode": "02" })
+        }
     });
+});
+
     router.post('/join', function(req, res) {
         var params = req.query;
         console.log('/join() \n',params);
@@ -54,7 +57,7 @@ router.get('/idCheck', function(req, res) {
                 mem_id: params.loginId,
                 mem_pw: params.password,
                 mem_name: params.name,
-                mem_email : params.email,
+                // mem_email : params.email,
                 mem_phone : params.phoneNumber,
                 mem_emailYn : 'N',
                 mem_smsYn : 'N'       
@@ -88,5 +91,42 @@ router.get('/idCheck', function(req, res) {
             }
         });
     });
+
+    router.get('/pwSearch',function(req,res){
+        var newPw = random.getRandom();
+        console.log('pwSearch || ',newPw);
+        // 휴대폰 인증 성공
+        if(1){
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: 'dev.hyeung@gmail.com'
+                  ,pass: 'clsdo123!'
+                  ,key : "AIzaSyBqZL0wYwBEJM0muC8-382rTmCrXh-XPjM"
+                }
+              });
+
+              var mailOptions = {
+                from: 'dev.hyeung@gmail.com',
+                to: 'phu8460@naver.com',
+                subject: 'MyRentCar 비밀번호 초기화',
+                text: '초기화 된 비밀번호는 '+newPw+" 입니다. 로그인 후 변경하시기 바랍니다."
+              };
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+
+        }else{
+        // 실패
+
+        }
+
+    });
+
 
 module.exports = router;
