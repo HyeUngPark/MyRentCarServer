7afad8fdf8d8f4688905bdf6e1f0015e2fb46358
HyeUngPark <phu8460@gmail.com>
2019-06-02 16:11:16 +0900

    google auth file adding

--- route/commonRouter.js
+++ route/commonRouter.js
@@ -20,6 +20,17 @@ router.get('/googlec9bbba10a3ea94c1.html', function(req, res) {
     // console.log("job ===\n",req.job);
     // res.json({login:'success'});
 });
+router.get('/google6d10baa83fea548d.html', function(req, res) {
+    console.log("/호출!!!");
+    fs.readFile('google6d10baa83fea548d.html',function(err, data){
+        res.writeHead(200, {'content-Type':'text/html'});
+        res.end(data)
+    });
+    // console.log("job ===\n",req.job);
+    // res.json({login:'success'});
+});
+
+
 
 
 
